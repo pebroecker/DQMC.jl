d550155057d7217430fb4c705adf265d848badcb
Peter Broecker <pebrbus@gmail.com>
2017-08-31 14:27:35 +0200

    Fixed typo

--- src/helper/convert_square_to_qlt.jl
+++ src/helper/convert_square_to_qlt.jl
@@ -5,22 +5,23 @@ L = parse(Int, ARGS[2])
 out_file = "qlt_$(in_file)"
 
 h = h5open(in_file, "r")
-# 64x2x25x2x3000
-data = zeros(L * L, 1, 25, 1, 32 * 128)
 
-idx = 1
+n_samples = 1
 
-for i in 1:32
+data = zeros(L * L, 1, 25, 1, n_samples * 128)
+
+
+for i in 1:n_samples
     greens = read(h, "simulation/results/greens_real/1")# + 1im * read(h, "simulation/results/greens_imag/1")
     neighs = [-L, -1, 0, 1, L]
 
     for site in 1:L * L
         ns = mod1.(site + neighs, L * L)
+
         for (k, m) in enumerate(ns)
             for (l, n) in enumerate(ns)
                 for sample in 1:128
-                    data[site, 1, 5 * (k - 1) + l, 1, idx] = greens[m, n, sample]
-                    idx += 1
+                    data[site, 1, 5 * (k - 1) + l, 1, (i - 1) * 128 + sample] = greens[m, n, sample]
                 end
             end
         end
